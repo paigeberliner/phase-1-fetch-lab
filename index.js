ae96dd56fb7c42a48dada561f2006727f48f584c
Paige <paigeberliner@gmail.com>
2023-08-21 19:38:16 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return(fetch('https://anapioficeandfire.com/api/books'))
+  .then((resp) => resp.json())
+  .then((data) => renderBooks(data));
 }
 
-function renderBooks(books) {
+function renderBooks(book) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  book.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
